316b127cdc57c8ae1aff1684772877313c245b50
SDolleris33 <sdolleris33@gmail.com>
2020-07-24 23:30:52 +0000

    revise main.js

--- main.js
+++ main.js
@@ -5,3 +5,35 @@ function ToggleNav(){
   
   }
 }
+
+const recipes = [
+ 'rum ,coke', 
+ '', 
+ '', 
+ '', 
+ '', 
+ '', 
+ '', 
+ '', 
+ '', 
+ '', 
+ '', 
+ '', 
+ '', 
+ '', 
+ '', 
+]
+  
+  function getRecipe(){
+  
+  let value = parsint(document.getElementById('cocktails').value);
+  return value;
+  
+  }
+  
+  function showRecipe(index){
+    document.getElementById('recipe_out').innerText = recipes[index];
+}
+
+showRecipe(getRecipe());
+  
